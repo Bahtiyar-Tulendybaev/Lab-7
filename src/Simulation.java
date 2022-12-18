03b6f1ca6e648e78e1bc58cae2cf18304d677b16
Bahtiyar Tulendybaev <bahtiyar_tulendybaev@mail.ru>
2022-12-18 19:13:20 +0600

    add new class Simulation and new enum Towns

---
+++ src/Simulation.java
@@ -0,0 +1,46 @@
+public class Simulation  implements Events{
+    @Override
+    public void anOrdinaryDay() {
+
+    }
+
+    @Override
+    public void rainyWeather() {
+
+    }
+
+    @Override
+    public void smoothRoad() {
+
+    }
+
+    @Override
+    public void theWheelBroke() {
+
+    }
+
+    @Override
+    public void river() {
+
+    }
+
+    @Override
+    public void iMetLocal() {
+
+    }
+
+    @Override
+    public void Highwaymen() {
+
+    }
+
+    @Override
+    public void RoadsideInn() {
+
+    }
+
+    @Override
+    public void theProductHasDeteriorated() {
+
+    }
+}
---
+++ src/Towns.java
@@ -0,0 +1,9 @@
+public enum Towns {
+    BISHKEK,
+    OSH,
+    BALYKCHY,
+    BATKEN,
+    KARAKOL,
+    NARYN,
+    TALAS
+}
