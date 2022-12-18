ccbeafdfb05f6c52e24bbc7b7b05ffd4cefe1bda
Bahtiyar Tulendybaev <bahtiyar_tulendybaev@mail.ru>
2022-12-18 19:11:29 +0600

    add new interface Events

---
+++ src/Events.java
@@ -0,0 +1,11 @@
+public interface Events {
+    void anOrdinaryDay();
+    void rainyWeather();
+    void smoothRoad();
+    void theWheelBroke();
+    void river();
+    void iMetLocal();
+    void Highwaymen();
+    void RoadsideInn();
+    void theProductHasDeteriorated();
+}
