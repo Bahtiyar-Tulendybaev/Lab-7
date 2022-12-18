386d7812a8ebc39dab66d1dfcf78f6773e541044
Bahtiyar Tulendybaev <bahtiyar_tulendybaev@mail.ru>
2022-12-18 19:12:46 +0600

    correct fields of class Seller

--- src/Seller.java
+++ src/Seller.java
@@ -2,4 +2,29 @@ public class Seller {
     private int loadCapacity;
     private int speed;
     private double money;
+
+    public int getLoadCapacity() {
+        return loadCapacity;
+    }
+
+    public void setLoadCapacity(int loadCapacity) {
+        this.loadCapacity = loadCapacity;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public double getMoney() {
+        return money;
+    }
+
+    public void setMoney(double money) {
+        this.money = money;
+    }
 }
+
